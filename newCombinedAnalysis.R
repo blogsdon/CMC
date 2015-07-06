fcf7cbbaa1351434d1da305b0af7948fccabc5fa
Ben Logsdon <aeberhart@gmail.com>
2015-07-06 11:51:58 -0700

    updates

--- newCombinedAnalysis.R
+++ newCombinedAnalysis.R
@@ -17,13 +17,18 @@ load(ppisyn@filePath)
 synResult <- synQuery('select name,id from file where projectId==\'syn3455058\' and networkStorageType==\'sparse\'')
 
 computePower <- function(net,true){
-  return(sum(net!=0 & true !=0)/(sum(true!=0)/2))
+  power <- sum(net!=0 & true !=0)/(sum(true!=0)/2)
+  cat(signif(power,2),'\n')
+  return(power)
+  
 }
 
-computeFDR <- function(net,true){
+computeTDR <- function(net,true){
   tp <- sum(net!=0 & true !=0)
   fp <- sum(net!=0 & true ==0)
-  return((fp)/(tp+fp))
+  tdr <- tp/(tp+fp)
+  cat(signif(tdr,2),'\n')
+  return(tdr)
 }
 
 allNets <- lapply(synResult$file.id,getAllNetworks)
--- sparsifyNetworks.sh
+++ sparsifyNetworks.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #scz no sva
-#Rscript /shared/metanetworkSynapse/makeMultiSparseNetwork.R "syn4546999" "syn4546233" "syn4550165" "syn3526289" "executed.txt" > test.sh
-#qsub -S /bin/bash -V -cwd -N aracne -e error.txt -o out.txt test.sh
+echo "Rscript /shared/metanetworkSynapse/makeMultiSparseNetwork.R "syn4546999" "syn4546233" "syn4550165" "syn3526289" "executed.txt"" > test.sh
+qsub -S /bin/bash -V -cwd -N aracne -e error.txt -o out.txt test.sh
 
 echo "Rscript /shared/metanetworkSynapse/makeMultiSparseNetwork.R "syn4546999" "syn4546344" "syn4550165" "syn3526289" "executed.txt"" > test.sh
 qsub -S /bin/bash -V -cwd -N correlation -e error.txt -o out.txt test.sh
@@ -13,8 +13,8 @@ qsub -S /bin/bash -V -cwd -N genie3 -e error.txt -o out.txt test.sh
 echo "Rscript /shared/metanetworkSynapse/makeMultiSparseNetwork.R "syn4546999" "syn4546464" "syn4550165" "syn3526289" "executed.txt"" > test.sh
 qsub -S /bin/bash -V -cwd -N lassoAIC -e error.txt -o out.txt test.sh
 
-#echo "Rscript /shared/metanetworkSynapse/makeMultiSparseNetwork.R "syn4546999" "syn4546491" "syn4550165" "syn3526289" "executed.txt"" > test.sh
-#qsub -S /bin/bash -V -cwd -N lassoBIC -e error.txt -o out.txt test.sh
+echo "Rscript /shared/metanetworkSynapse/makeMultiSparseNetwork.R "syn4546999" "syn4546491" "syn4550165" "syn3526289" "executed.txt"" > test.sh
+qsub -S /bin/bash -V -cwd -N lassoBIC -e error.txt -o out.txt test.sh
 
 echo "Rscript /shared/metanetworkSynapse/makeMultiSparseNetwork.R "syn4546999" "syn4546372" "syn4550165" "syn3526289" "executed.txt"" > test.sh
 qsub -S /bin/bash -V -cwd -N lassoCV1se -e error.txt -o out.txt test.sh
