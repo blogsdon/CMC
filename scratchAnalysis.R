3d9263d699ebf561dbd3f64823b1ac816f87d2fb
Ben Logsdon <aeberhart@gmail.com>
2015-07-21 16:23:09 -0700

    test analysis

---
+++ scratchAnalysis.R
@@ -0,0 +1,30 @@
+#new prototype analysis
+
+require(synapseClient)
+synapseLogin()
+
+a <- synGet('syn4553095')
+load(a@filePath)
+net <- as.matrix(sparseNetwork)
+ne <- rowSums(net)
+
+
+ne2 <- which.max(ne)
+getNeighb <- function(index,x){
+  return(which(x[index,]))
+}
+
+neighb <- getNeighb(ne2,net)
+neighb2 <- unique(unlist(lapply(neighb,getNeighb,net)))
+neighb3 <- unique(c(ne2,neighb2,neighb))
+neighb4 <- unique(unlist(lapply(neighb3,getNeighb,net)))
+neighb5 <- unique(c(neighb4,neighb3))
+#neighb <- which(net[9171,])
+
+b <- synGet('syn4550165')
+key <- read.csv(b@filePath)
+
+
+
+cat(as.matrix(key)[neighb5,2],file='~/Desktop/test.csv',sep='\n')
+#b <- kmeans(net[1:1000,1:1000],centers = 20)
