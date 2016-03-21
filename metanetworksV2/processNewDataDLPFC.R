ba4fa6071ab6ffedc069257846673d916970893c
Benjamin Logsdon <aeberhart@gmail.com>
2016-03-21 11:55:56 -0700

    cmc dlpfc network submission scripts

---
+++ metanetworksV2/processNewDataDLPFC.R
@@ -0,0 +1,43 @@
+#login to synapse client
+require(synapseClient)
+library(dplyr)
+library(utilityFunctions)
+
+synapseLogin()
+
+covariatesObj <- synGet('syn5570053')
+residualExpressionObj <- synGet('syn5570051')
+
+library(data.table)
+library(utilityFunctions)
+cmcCovariates <- fread(covariatesObj@filePath,data.table=F,stringsAsFactors=F)
+cmcExpression <- fread(residualExpressionObj@filePath,data.table=F,stringsAsFactors=F)
+
+cmcExpression2 <- t(cmcExpression[,-c(1)])
+colnames(cmcExpression2) <- cmcExpression$GeneFeature
+cmcCovariates <- cmcCovariates[,-86]
+
+#Bipolar
+BipolarCov <- dplyr::filter(cmcCovariates,Dx=='AFF')
+
+cmcExpression2[dplyr::filter(cmcCovariates,Dx=='AFF')$`DLPFC_RNA_isolation: Sample RNA ID`,] %>%
+  apply(2,utilityFunctions::winsorize) %>%
+  scale %>%
+  write.csv(file='cmcDLPFCBipolarRNASeq.csv',quote=F)
+
+#MCI
+cmcExpression2[dplyr::filter(cmcCovariates,Dx=='SCZ')$`DLPFC_RNA_isolation: Sample RNA ID`,] %>%
+  apply(2,utilityFunctions::winsorize) %>%
+  scale %>%
+  write.csv(file='cmcDLPFCSchizophreniaRNASeq.csv',quote=F)
+
+#AD
+cmcExpression2[dplyr::filter(cmcCovariates,Dx=='Control')$`DLPFC_RNA_isolation: Sample RNA ID`,] %>%
+  apply(2,utilityFunctions::winsorize) %>%
+  scale %>%
+  write.csv(file='cmcDLPFCControlRNASeq.csv',quote=F)
+
+cmcExpression2 <- apply(cmcExpression2,2,utilityFunctions::winsorize)
+cmcExpression2 <- scale(cmcExpression2)
+
+write.csv(cmcExpression2,file='cmcDLPFCRNASeq.csv',quote=F)
